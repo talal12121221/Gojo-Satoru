126e7c9b1dbd5c7de929324c9cb048fa525e5de0
talal12121221 <117723715+talal12121221@users.noreply.github.com>
2022-11-09 19:08:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['96891103640']
-global.premium = ['96891103640']
-global.ownernomer = '96891103640'
+global.owner = ['966504124495']
+global.premium = ['966504124495']
+global.ownernomer = '966504124495'
 global.ownername = '( الـمـطـور )'
-global.botname = '( بـوت غـوجـو )'
-global.footer = '( غـوجـو )'
+global.botname = '( 𝗥𝗔𝗶 | 𝗧𝗔𝗟𝗔𝗟💙 )'
+global.footer = '( 𝗥𝗔𝗶 | 𝗧𝗔𝗟𝗔𝗟 )'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://chat.whatsapp.com/CKLmAGJo3iw5AvNM1KZee3'
 global.myweb = 'https://youtube.com/channel/UCPvrnWknQrgCSIeobXMP0Uw'
 global.packname = 'https://Wa.Me/+96891103640'
-global.author = '[ غـوجـو ]'
+global.author = '[ 𝗥𝗔𝗶 | 𝗧𝗔𝗟𝗔𝗟 ]'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
